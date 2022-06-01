91f98d216b19de535c74496e9901aa614acf1bc7
Matt Pocock <mattpocockvoice@gmail.com>
2022-06-01 07:19:08 +0200

    Added comment

--- starters/vanilla/src/main.ts
+++ starters/vanilla/src/main.ts
@@ -4,11 +4,14 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 const machine = createMachine({});
 
+/**
+ * Runs the machine and shows it on screen
+ */
 interpret(machine)
   .start()
   .subscribe((state) => {
     app.innerHTML = `
-        <code>${state.value}</code>
-        <pre>${JSON.stringify(state.context, null, 2)}</pre>
-      `;
+      <code>${state.value}</code>
+      <pre>${JSON.stringify(state.context, null, 2)}</pre>
+    `;
   });
