38eb6fff3d03c9bc3a4d80d04b7d05df107ce760
Matt Pocock <mattpocockvoice@gmail.com>
2022-06-01 07:23:39 +0200

    Stripped out node-lambda starter

--- starters/node-lambda/src/main.ts
+++ starters/node-lambda/src/main.ts
@@ -1,86 +1,24 @@
 import { createMachine, interpret } from "xstate";
 import { waitFor } from "xstate/lib/waitFor";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
-} from "aws-lambda";
-
-const handlerMachine = createMachine(
-  {
-    tsTypes: {} as import("./main.typegen").Typegen0,
-    context: {
-      name: "",
-    },
-    schema: {
-      services: {
-        getUser: {
-          data: {} as {},
-        },
-      },
-    },
-    id: "Handler machine",
-    initial: "gettingUser",
-    states: {
-      gettingUser: {
-        invoke: {
-          src: "getUser",
-          onDone: [
-            {
-              target: "User found",
-            },
-          ],
-          onError: [
-            {
-              target: "Not found",
-            },
-          ],
-        },
-      },
-      "User found": {
-        type: "final",
-      },
-      "Not found": {
-        type: "final",
-      },
-    },
-  },
-  {
-    services: {
-      getUser: async (context) => {
-        if (context.name !== "mattpocock") {
-          throw new Error("User not found");
-        }
-        return Promise.resolve({});
-      },
-    },
-  },
-);
+const handlerMachine = createMachine({});
 
 export const lambdaHandler = async (
-  event: APIGatewayProxyEvent,
+  event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   const actor = interpret(
     handlerMachine.withContext({
       name: event.queryStringParameters.name,
-    }),
+    })
   ).start();
 
-  const finalState = await waitFor(
-    actor,
-    (state) => state.done,
-  );
+  const finalState = await waitFor(actor, (state) => state.done);
 
-  if (finalState.matches("User found")) {
+  if (finalState.matches("success")) {
     return {
       statusCode: 200,
-      body: `Found!`,
-    };
-  }
-  if (finalState.matches("Not found")) {
-    return {
-      statusCode: 404,
-      body: "Not found",
+      body: `Success`,
     };
   }
   return {
